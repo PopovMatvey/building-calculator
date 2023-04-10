05ac2b4d93d74e0c938570a4e388df903c3a17e8
матвей попов <matyha550@gmail.com>
2023-04-10 14:02:20 +0300

    calculator v 0.0.1

---
+++ client/src/assets/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './css/style.css';
+import { HeaderInformation } from "../HeaderInformation";
+
+export function Header(){
+
+    return(
+        <header>
+            <HeaderInformation/>
+        </header>
+    );
+}
