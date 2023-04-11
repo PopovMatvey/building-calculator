1ba159373b15b9bd2c7ee7ff364a608913d4c745
матвей попов <matyha550@gmail.com>
2023-04-11 09:44:51 +0300

    calculator v 0.0.3

--- client/src/assets/components/MainContent/index.tsx
+++ client/src/assets/components/MainContent/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./css/style.css";
+import { RadioItem } from "../RadioItem";
+import { ItemContainer } from "../ItemContainer";
 
 // Основной контент
 export function MainContent() {
@@ -8,6 +10,14 @@ export function MainContent() {
         <>
             <div className="main-content_container">
                 <h1>Калькулятор</h1>
+                <ItemContainer text={"Первый блок"}>
+                    <RadioItem nameRadioButton={"1"} checkedButton={true} />
+                    <RadioItem nameRadioButton={"1"} />
+                </ItemContainer>
+                <ItemContainer text={"Dnjhjq ,kjr"}>
+                    <RadioItem nameRadioButton={"2"} checkedButton={true} />
+                    <RadioItem nameRadioButton={"2"} />
+                </ItemContainer>
             </div>
         </>
     )
